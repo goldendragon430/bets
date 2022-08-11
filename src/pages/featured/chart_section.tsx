1085c8de82ae9acdf91022b1b3cc69fecdf6ec0b
atroboj <atroboj@gmail.com>
2022-08-11 13:59:47 -0400

    feat: update the roulette pieces

--- src/pages/featured/chart_section.tsx
+++ src/pages/featured/chart_section.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-array-index-key */
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import styled from 'styled-components';
 
@@ -7,8 +7,6 @@ import EthIcon from '../../assets/images/eth_icon.svg';
 import Input from '../../components/common/input';
 import { Typography, TypographyType } from '../../components/common/typography';
 import { useTheme } from '../../contexts/theme_context';
-
-const PIECE_COUNT = 10;
 
 const Container = styled.div`
   position: relative;
@@ -64,14 +62,19 @@ const ChartWrapper = styled(Circle)`
 
   width: 70%;
   height: 70%;
-  animation: rotating 20s linear infinite;
+  // animation: rotating 20s linear infinite;
+`;
+
+const PieBg = styled(Circle)<{ color: string }>`
+  width: 100%;
+  height: 100%;
+  background: ${({ color }) => color};
 `;
 
 const PiePiece = styled(Circle)<{ color: string; value: number; angle: number }>`
   width: 100%;
   height: 100%;
   background: ${({ color }) => color};
-  box-shadow: inset 0px 4px 33px ${({ theme }) => theme.colors.grey1};
   clip-path: polygon(50% 50%, 50% 0%, ${({ value }) => 50 + 50 * Math.tan((value * Math.PI) / 180)}% 0%);
   transform: translate(-50%, -50%) rotate(${({ angle }) => `${angle}deg`});
 `;
@@ -80,11 +83,18 @@ const PieLine = styled.div<{ angle: number }>`
   position: absolute;
   top: 0;
   left: 50%;
-  height: 100%;
+  height: 50%;
   background: ${({ theme }) => `${theme.colors.grey1}10`};
   border: 1px solid ${({ theme }) => `${theme.colors.grey1}20`};
   box-shadow: 0px 0px 11px ${({ theme }) => theme.colors.grey1}, inset 0px 0px 11px ${({ theme }) => theme.colors.grey1};
   transform: rotate(${({ angle }) => `${angle}deg`});
+  transform-origin: bottom center;
+`;
+
+const PieShadow = styled(Circle)`
+  width: 100%;
+  height: 100%;
+  box-shadow: inset 0px 4px 33px ${({ theme }) => theme.colors.grey1};
 `;
 
 const ChartFrame3 = styled(Circle)`
@@ -153,28 +163,46 @@ const ChartSection: React.FC = () => {
 
   const [value, setValue] = useState('50');
 
-  const getDataByIndex = (index: number) => {
+  const getPieceCount = useCallback(() => {
     const redValue = Math.max(Math.min(Number(value) || 0, 100), 0);
-    const pieceAngle = 360 / PIECE_COUNT;
+    if (redValue <= 12.5 || redValue >= 87.5) {
+      return 2;
+    }
+    if (redValue <= 25 || redValue >= 75) {
+      return 4;
+    }
+    return 8;
+  }, [value]);
 
-    // const redCount = Math.round((redValue / 100) * PIECE_COUNT);
-    // const blueCount = PIECE_COUNT - redCount;
+  const getDataByIndex = useCallback(
+    (index: number) => {
+      let redValue = Math.max(Math.min(Number(value) || 0, 100), 0);
+      redValue = Math.max(Math.min(redValue, 96), 4);
 
-    // const smallCount = Math.min(redCount, blueCount);
-    // const colorA = redCount < blueCount ? theme.colors.red1 : theme.colors.blue1;
-    // const colorB = redCount < blueCount ? theme.colors.blue1 : theme.colors.red1;
+      const pieceAngle = 360 / getPieceCount();
+      const isRed = redValue > 50;
 
-    // // eslint-disable-next-line no-nested-ternary
-    // const color = index < smallCount * 2 && index % 2 === 0 ? colorA : colorB;
+      const multiplier = 8 / getPieceCount();
 
-    return {
-      value: index % 2 === 0 ? (pieceAngle / 100) * redValue : (pieceAngle / 100) * (100 - redValue),
-      color: index % 2 === 0 ? theme.colors.red1 : theme.colors.blue1,
-      angle: pieceAngle * Math.floor(index / 2) + (index % 2 === 0 ? 0 : (pieceAngle / 100) * redValue),
-    };
-  };
+      redValue = (isRed ? redValue - (100 - 100 / multiplier) : redValue) * multiplier;
 
-  const data = new Array(PIECE_COUNT * 2).fill(0).map((_, index) => getDataByIndex(index));
+      const color = index % 2 === 0 ? theme.colors.red1 : theme.colors.blue1;
+      const pieceValue = index % 2 === 0 ? (pieceAngle / 100) * redValue : (pieceAngle / 100) * (100 - redValue);
+      const angle = pieceAngle * Math.floor(index / 2) + (index % 2 === 0 ? 0 : (pieceAngle / 100) * redValue);
+
+      return {
+        color,
+        value: pieceValue / multiplier,
+        angle: angle / multiplier,
+      };
+    },
+    [value, theme, getPieceCount]
+  );
+
+  const data = useMemo(
+    () => new Array(getPieceCount() * 2).fill(0).map((_, index) => getDataByIndex(index)),
+    [getPieceCount, getDataByIndex]
+  );
 
   const handleChangeValue = (e: any) => {
     const res = e.target.value;
@@ -192,12 +220,14 @@ const ChartSection: React.FC = () => {
         <ChartFrame1 />
 
         <ChartWrapper>
+          <PieBg color={Number(value) > 50 ? theme.colors.red1 : theme.colors.blue1} />
           {data.map((item, index) => (
             <PiePiece angle={item.angle} color={item.color} key={index} value={item.value} />
           ))}
           {data.map((item, index) => (
             <PieLine angle={item.angle} key={index} />
           ))}
+          <PieShadow />
         </ChartWrapper>
 
         <ChartFrame3 />
